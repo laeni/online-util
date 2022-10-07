4e0b1e220c15e7c514d1cd3805ff186a3b2f885f
Laeni <m@laeni.cn>
2022-10-07 11:16:42 +0800

    fix: 修复在子页面刷新404问题

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // 尾斜杠 - 将'/xxx.html'输出为'/xxx/index.html',以解决导出静态文件后直接访问子页面404的问题
+  trailingSlash: true,
   swcMinify: true,
 }
 
