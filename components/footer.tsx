f17e3ddf8c1af5ee8054a8e5728419713986d93d
Laeni <m@laeni.cn>
2023-03-16 09:39:03 +0800

    feat: 增加Github项目链接

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,5 @@
+import { GithubOutlined } from '@ant-design/icons';
+
 /**
  * 页脚.
  */
@@ -5,10 +7,16 @@ export default function Footer() {
   const year = new Date().getFullYear();
   return (
     <div className="flex-none relative z-50 leading-3 ring-1 ring-gray-900 ring-opacity-5 shadow-sm py-6 text-xs sm:text-sm text-gray-400">
-      <div className="flex justify-center">
+      <div className="flex justify-center items-center">
         <div className="px-2">
           <span>{`© ${year !== 2022 ? '2022-' : ''}${year} All Right Reserved `}</span>
           <a target="_black" href="https://beian.miit.gov.cn/">滇ICP备17005647号-2</a>
+        </div>
+        {/* github */}
+        <div className="mx-0 sm:mx-2">
+          <a href="https://github.com/laeni/online-util" target="_blank">
+            <GithubOutlined />
+          </a>
         </div>
       </div>
     </div>
